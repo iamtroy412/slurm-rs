39eda0c23c141d0f1a09f0f614d11c54ea8d3338
Troy Caro <iamtroy412@gmail.com>
2023-04-02 17:42:01 -0400

    add: create Slurm struct from environment variables.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! For more information, the Slurm REST API is documented at
 //! <https://slurm.schedmd.com/rest_api.html>
 use reqwest::Client;
-use std::sync::Arc;
+use std::{env, sync::Arc};
 
 /// Entrypoint for interacting with the API.
 /// To authenticate with the API, we need a user and a token.
@@ -25,15 +25,24 @@ impl Slurm {
     {
         let client = Client::builder().build();
         match client {
-            Ok(c) => {
-                return Slurm {
-                    user: user.to_string(),
-                    token: token.to_string(),
-                    client: Arc::new(c),
-                }
-            }
+            Ok(c) => Slurm {
+                user: user.to_string(),
+                token: token.to_string(),
+                client: Arc::new(c),
+            },
             Err(e) => panic!("Unable to create client: {e:?}"),
         }
+    }
+
+    /// Create a new Slurm client struct from environment variables.
+    /// It takes any type that can convert into a &str.
+    /// Since this lib is useless withouth a client to connect with, this
+    /// will panic if creating a client fails.
+    pub fn new_from_env() -> Self {
+        let user = env::var("X-SLURM-USER-NAME").expect("X-SLURM-USER-NAME should be set!");
+        let token = env::var("X-SLURM-USER-TOKEN").expect("X-SLURM-USER-TOKEN should be set!");
+
+        Slurm::new(user, token)
     }
 }
 
