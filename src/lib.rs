8cd5b370e9c56d02dec3a814d7aed451b0a6314b
Troy Caro <iamtroy412@gmail.com>
2023-04-19 10:00:51 -0400

    fix: slurmdb request path construction and env variable names

--- src/lib.rs
+++ src/lib.rs
@@ -163,14 +163,17 @@ impl SlurmDB {
         }
     }
 
-    /// Create a new Slurm client struct from environment variables.
+    /// Create a new SlurmDB client struct from environment variables.
     /// It takes any type that can convert into a &str.
     /// Since this lib is useless withouth a client to connect with, this
     /// will panic if creating a client fails.
     pub fn new_from_env() -> Self {
-        let endpoint = env::var(SLURM_ENDPOINT).expect("{SLURM_ENDPOINT} should be set!");
-        let user = env::var(SLURM_USER).expect("{SLURM_USER} should be set!");
-        let token = env::var(SLURM_TOKEN).expect("{SLURM_TOKEN} should be set!");
+        let endpoint =
+            env::var("X_SLURM_ENDPOINT").expect("env variable 'X_SLURM_ENDPOINT' should be set");
+        let user =
+            env::var("X_SLURM_USER_NAME").expect("env variable 'X_SLURM_USER_NAME' should be set");
+        let token = env::var("X_SLURM_USER_TOKEN")
+            .expect("env variable 'X_SLURM_USER_TOKEN' should be set");
 
         SlurmDB::new(user, token, endpoint)
     }
@@ -187,9 +190,8 @@ impl SlurmDB {
         B: Serialize,
     {
         // https://slurm-endpoint/{slurm,slurmdb}/v0.0.38/{nodes, diag, etc..}
-        let url = self.endpoint.join("slurmdb")?;
-        let url = url.join(SLURM_API_VERSION)?;
-        let url = url.join(path)?;
+        let url_path = format!("slurm/{}/{}", SLURM_API_VERSION, path);
+        let url = self.endpoint.join(&url_path)?;
 
         // Build auth headers
         let user_header_name =
